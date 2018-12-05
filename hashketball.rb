223a03efbda947cf9b38b46636c7c45a79477225
SOULEYMANE SOW <souleymanesow@gmail.com>
2018-12-05 14:38:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,8 +1,43 @@
 # Write your code here!
 
+ def game_hash
+  hash ={
+    
+    :home => {
+      # :team_name =>"",
+      # :colors =>["",""]
+      # :players => { }
+        
+        
+    }
+  }
+      
+# },
+    :away => {
+#       :team_name =>"",
+#       :colors    =>["",""]
+#       :players   => {}
+  
+    }
+    
+}
+end 
 
 
-
+#         "name" => "",
+#         stats {
+           
+#             :number => ,
+#             :shoe => ,
+#             :points => ,
+#             :rebounds => ,
+#             :assists => ,
+#             :steals => ,
+#             :blocks => ,
+#             :slam_dunks => ,
+#         }
+    # }
+      
 
 
 
